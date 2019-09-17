4de4a4f3356ab95957fb2420d9e85d44257f59e8
Ryan McKenzie <Ryan@office.clubready.local>
2019-09-16 19:34:09 -0500

    Add project files.

---
+++ SimpleCrm.Web/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleCrm.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IGreeter, ConfigurationGreeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            IGreeter greeter)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                {
+                    ExceptionHandler = (context) =>
+                    {
+                       return context.Response.WriteAsync("Oops!");
+                    }
+                });
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(ConfigureRoutes);
+        }
+        private void ConfigureRoutes(IRouteBuilder routes) 
+          {
+            routes.MapRoute("default",
+                "A/{controller}/B/{action}");
+           }
+    }
+}
